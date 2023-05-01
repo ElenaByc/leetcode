0374e80aa948a1834e76ba8dee5395e2e70a9640
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-04-30 23:03:53 -0700

    Time: 43 ms (7.41%), Space: 16.3 MB (5.07%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,7 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        curr = [1] * n
+        for i in range (1, m):
+            for j in range (1, n):
+                curr[j] += curr[j - 1]
+        return curr[n - 1]
