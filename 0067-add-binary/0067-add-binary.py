34e4f508e2e3ab6bdddfcaf41fed848d0c3e9b91
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-05-21 22:53:36 -0700

    Time: 57 ms (9.18%), Space: 16.3 MB (30.76%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -4,24 +4,26 @@ class Solution:
         max_len = max(len(a), len(b))
         a = a.zfill(max_len)
         b = b.zfill(max_len)
-        
+        a = list(map(lambda x: int(x), list(a)))
+        b = list(map(lambda x: int(x), list(b)))
+
         carry = 0
         for i in range(max_len - 1, -1, -1):
-            sum = int(a[i]) + int(b[i]) + carry
+            sum = a[i] + b[i] + carry
             if sum < 2:
-                result.append(str(sum))
+                result.append(sum)
                 carry = 0
             elif sum == 2:
-                result.append("0")
+                result.append(0)
                 carry = 1
             else: 
-                result.append("1")
+                result.append(1)
                 carry = 1
         if carry == 1:
-            result.append("1")
+            result.append(1)
         
         result.reverse()
 
-        return "".join(result)
+        return "".join(list(map(lambda x: str(x), result)))
             
             
