3509c65db45528279d749a011114138da904cf1f
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-07-11 21:07:09 -0700

    Time: 1 ms (10.46%), Space: 40.4 MB (71.18%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
@@ -19,32 +19,24 @@ class Solution {
             return 0;
         }
         
-        int sum = 0;
-        ArrayList<TreeNode> level = new ArrayList<>();
-        level.add(root);
         
-        ArrayList<TreeNode> nextLevel = new ArrayList<>();
-        TreeNode left, right;
-        
-        while (level.size() > 0) {
-            for (TreeNode node : level) {
-                left = node.left;
-                if (left != null) {
-                    if (left.left == null && left.right == null) { //leaf
-                        sum += left.val;
-                    } else {
-                        nextLevel.add(left);
-                    }
-                }
-                right = node.right;
-                if (right != null) {
-                    nextLevel.add(right);
-                }
+        int ans = 0;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while(!stack.empty()) {
+            TreeNode node = stack.pop();
+            if(node.left != null) {
+                if (node.left.left == null && node.left.right == null)
+                    ans += node.left.val;
+                else
+                    stack.push(node.left);
             }
-            level.clear();
-            for(TreeNode node: nextLevel) level.add(node);
-            nextLevel.clear();
+            if(node.right != null) {
+                if (node.right.left != null || node.right.right != null)
+                    stack.push(node.right);
+            }
         }
-        return sum;
+        return ans;
     }
 }
