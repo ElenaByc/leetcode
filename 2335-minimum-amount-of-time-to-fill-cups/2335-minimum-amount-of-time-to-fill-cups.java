fd8f379994a25caa1bd8f014443e19f35cdfb21e
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-08-01 18:16:49 -0700

    Time: 1 ms (79.13%), Space: 40.3 MB (36.73%) - LeetHub

--- 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.java
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.java
@@ -1,24 +1,15 @@
 class Solution {
     public int fillCups(int[] amount) {
-        int coldcups = amount[0];
-        int warmcups = amount[1]; 
-        int hotcups = amount[2];
+        Arrays.sort(amount);
         int minimumSeconds = 0;
 
-        while (coldcups > 0 || warmcups > 0 || hotcups > 0) {
-            if (coldcups <= warmcups && coldcups <= hotcups) {
-                warmcups--;
-                hotcups--;
-            } else if (warmcups <= coldcups && warmcups <= hotcups) {
-                hotcups--;
-                coldcups--;
-            } else {
-                warmcups--;
-                coldcups--;
-            }
-            minimumSeconds++;
+        while(amount[1] != 0){
+          amount[1]--;
+          amount[2]--;
+          minimumSeconds++;
+          Arrays.sort(amount);
         }
 
-        return minimumSeconds;
+        return minimumSeconds + amount[2];
     }
 }
