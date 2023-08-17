68221ca0b365d41ed712299e53d3ef41600c4efc
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-08-16 17:33:49 -0700

    Time: 13 ms (98.79%), Space: 53.9 MB (98.79%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.java
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.java
@@ -1,37 +1,17 @@
 class Solution {
     public int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
         int numOfFullBags = 0;
-        int freeSpaces = 0;
+        // int freeSpaces = 0;
 
         for (int i = 0; i < capacity.length; i++) {
             capacity[i] -= rocks[i];
-            freeSpaces += capacity[i];
+            // freeSpaces += capacity[i];
         }
 
         // if (freeSpaces <= additionalRocks) {
         //     return capacity.length;
         // }
 
-
-//         HashMap<Integer, Integer> hm = new HashMap<>();
-
-//         for (int num : capacity) { // O(n)
-//             hm.put(num, hm.getOrDefault(num, 0) + 1);
-//         }
-        
-//         for (int i = 0; i <= additionalRocks; i++) {
-//             if (hm.containsKey(i)) {
-//                 if (i == 0) {
-//                     numOfFullBags += hm.get(0);
-//                 } else if (i * hm.get(i) <= additionalRocks) {
-//                     additionalRocks -= i * hm.get(i);
-//                     numOfFullBags += hm.get(i);
-//                 } else {
-//                     numOfFullBags += additionalRocks / i;
-//                     break;
-//                 }
-//             }
-//         }
 
         Arrays.sort(capacity);
         for (int num: capacity) {
