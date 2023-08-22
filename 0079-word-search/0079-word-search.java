acf0f2b6398c73c7d618d0db2b4b94fb7bafcba0
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-08-21 18:47:10 -0700

    Time: 92 ms (97.06%), Space: 40.7 MB (54.33%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,28 +1,29 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
+        char[] wordArray = word.toCharArray();
         for (int i = 0; i < board.length; i++)
             for (int j = 0; j < board[0].length; j++) {
-                if (findWord(board, i, j, word, 0)) {
+                if (findWord(board, i, j, wordArray, 0)) {
                     return true;
                 }
             }
         return false;
     }
 
-    public static boolean findWord(char[][] board, int i, int j, String word, int ind) {
-        if (ind == word.length()) {
+    public static boolean findWord(char[][] board, int i, int j, char[] wordArray, int ind) {
+        if (ind == wordArray.length) {
             return true;
         }
         if (i > board.length - 1 || i < 0 || j < 0 || j > board[0].length - 1
-                || board[i][j] != word.charAt(ind)) {
+                || board[i][j] != wordArray[ind]) {
             return false;
         }
         board[i][j] = '*';
-        boolean result = findWord(board, i - 1, j, word, ind + 1) ||
-                findWord(board, i, j - 1, word, ind + 1) ||
-                findWord(board, i, j + 1, word, ind + 1) ||
-                findWord(board, i + 1, j, word, ind + 1);
-        board[i][j] = word.charAt(ind); //restore board
+        boolean result = findWord(board, i - 1, j, wordArray, ind + 1) ||
+                findWord(board, i, j - 1, wordArray, ind + 1) ||
+                findWord(board, i, j + 1, wordArray, ind + 1) ||
+                findWord(board, i + 1, j, wordArray, ind + 1);
+        board[i][j] = wordArray[ind]; //restore board
         return result;
     }
 }
