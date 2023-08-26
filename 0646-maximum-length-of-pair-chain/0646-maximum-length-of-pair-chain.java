5c7321d7c8462d1e5ea0908be31dfc81d658ddef
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-08-25 18:19:00 -0700

    Time: 8 ms (99.48%), Space: 43.8 MB (72.14%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
@@ -4,9 +4,10 @@ class Solution {
         int counter = 0;
         int i = 0;
         int n = pairs.length;
+        int curEnd;
         while (i < n) {
             counter++;
-            int curEnd = pairs[i][1];
+            curEnd = pairs[i][1];
             // look for the next pair
             while (i < n && pairs[i][0] <= curEnd) {
                 i++;
