ba550f9c22657b8cdb025f6dd28ce95baedbf887
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-09-07 20:16:35 -0700

    Time: 2 ms (82.29%), Space: 43.6 MB (61.83%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -5,15 +5,12 @@ class Solution {
         int[] res = new int[2];
         
         for (int i = 0; i < n; i++) {
-            hm.put(target - nums[i], i);
-        }
-        
-        for (int i = 0; i < n; i++) {
             if (hm.containsKey(nums[i]) && hm.get(nums[i]) != i) {
                 res[0] = i;
                 res[1] = hm.get(nums[i]);
                 return res;
-            }
+            } 
+            hm.put(target - nums[i], i);
         }
         
         return res;
