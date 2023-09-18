64e949a3be43501bb1f15fc5670076078b858dd6
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-09-17 20:41:20 -0700

    Time: 1 ms (99.31%), Space: 44.6 MB (59.35%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -1,47 +1,34 @@
 class Solution {
     public int[] kWeakestRows(int[][] mat, int k) {
-        int m = mat.length; // number of rows
-        int n = mat[0].length;
+        int m = mat.length;    // number of rows
+        int n = mat[0].length; // number of columns
         int[] res = new int[k];
-        int[] sums = new int[m];
-        HashMap<Integer, List<Integer>> hm = new HashMap<>();
-        List<Integer> list;
-        int sum;
-        
+        /*
+		 * we can create a score to match the sort condition from description
+		 * score = soldiersCount * rows + currentRowIndex
+		 * so we can get soldiersCount by score / rows, and get rowIndex by score % rows
+		 */
+        int[] score = new int[m];
+        int j;
         for (int i = 0; i < m; i++) {
-            sum = 0;
-            for (int j = 0; j < n; j++) {
-                sum += mat[i][j];
-            }
-            if (hm.containsKey(sum)) {
-                hm.get(sum).add(i);
-            } else {
-                list = new ArrayList<>();
-                list.add(i);
-                hm.put(sum, list);
-            }
-            sums[i] = sum;
-        }
-
-        Arrays.sort(sums);
-        // System.out.println(hm);
-        
-        int i = 0;
-        int j = 0;
-        while (i < k) {
-            if (hm.containsKey(sums[j])) {
-                list = hm.get(sums[j]);
-                hm.remove(sums[j]);
-                // System.out.println(list);
-                for (int a = 0; a < list.size() && i < k; a++) {
-                    res[i] = list.get(a);
-                    i++;
+            for (j = 0; j < n; j++) {
+                if (mat[i][j] == 0) {
+                    score[i] = j * m + i;
+                    break;
                 }
             }
-            j++;
+            if (j == n) {
+                score[i] = j * m + i;
+            }
         }
-    
-    
+        // System.out.println(Arrays.toString(score));
+        Arrays.sort(score);
+        // System.out.println(Arrays.toString(score));
+        for (int i = 0; i < k; i++) {
+            // get row Index
+            res[i] = score[i] % m;
+        }
+        
         return res;
     }
 }
