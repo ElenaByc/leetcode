38ce9294ee8f0931fc2cbbde4f32ac890b3a60f6
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-09-25 17:04:52 -0700

    Time: 0 ms (100.00%), Space: 41.9 MB (61.18%) - LeetHub

---
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int prefixCount(String[] words, String pref) {
+        int res = 0;
+        for (String s : words) {
+            if (s.indexOf(pref) == 0) {
+                res++;
+            }
+        }
+        return res;
+    }
+}
