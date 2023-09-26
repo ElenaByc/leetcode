fe3ec858e69bf69763bb1269a4b38ed97da06286
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-09-25 18:20:07 -0700

    Time: 3 ms (59.82%), Space: 41.1 MB (78.69%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -1,34 +1,33 @@
 class Solution {
     public String removeDuplicateLetters(String s) {
-        List<Character> list = new ArrayList<>();
+        Stack<Character> stack = new Stack<>();
         char ch;
-        boolean[] taken = new boolean[26];
-        int[] dict = new int[26];
+        boolean[] taken = new boolean[26]; // if character is present in current result Stack
+        int[] dict = new int[26]; // number of occurences of character
 
         // count letters
         for (int i = 0; i < s.length(); i++) {
             dict[s.charAt(i) - 'a']++;
         }
-        
-        System.out.println(Arrays.toString(dict));
 
         for(int i = 0; i < s.length(); i++) {
             ch = s.charAt(i);
             dict[ch - 'a']--;
-            if(taken[ch - 'a']) continue;
-            while (list.size() > 0 && 
-                   list.get(list.size() - 1) > ch && 
-                   dict[list.get(list.size() - 1) - 'a'] > 0 ) {
-                taken[list.get(list.size() - 1) - 'a'] = false;
-                list.remove(list.size() - 1);
+            if(taken[ch - 'a']) { //if character is already present in stack, dont bother
+                continue;
+            } 
+            while (!stack.isEmpty() && 
+                   stack.peek() > ch && 
+                   dict[stack.peek() - 'a'] > 0 ) {
+                taken[stack.pop() - 'a'] = false;
             }
-            list.add(s.charAt(i));
-            taken[s.charAt(i) - 'a'] = true;
+            stack.push(ch);
+            taken[ch - 'a'] = true;
         }
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < list.size(); i++) {
-            sb.append(list.get(i));
+        while(!stack.isEmpty()){
+            sb.insert(0,stack.pop());
         }
         return sb.toString();
     }
