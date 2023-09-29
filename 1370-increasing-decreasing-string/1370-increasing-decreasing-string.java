d4ace3e1d4b1bfe4248993051259ae7a736dfec6
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-09-28 19:58:35 -0700

    Time: 2 ms (100.00%), Space: 41.6 MB (98.80%) - LeetHub

--- 1370-increasing-decreasing-string/1370-increasing-decreasing-string.java
+++ 1370-increasing-decreasing-string/1370-increasing-decreasing-string.java
@@ -2,45 +2,29 @@ class Solution {
     public String sortString(String s) {
         int n = s.length();
         char ch;
-        int val;
-        HashMap<Character, Integer> hm = new HashMap<>();
         StringBuilder sb = new StringBuilder();
+        char[] arr = new char[26];
         
         for (int i = 0; i < n; i++) {
             ch = s.charAt(i);
-            val = hm.getOrDefault(ch, 0);
-            val++;
-            hm.put(ch, val);
+            arr[ch - 'a']++;
         }
         
         
         while (sb.length() < n) {
-            for (ch = 'a'; ch <= 'z'; ch++) {
-                if (hm.containsKey(ch)) {
-                    sb.append(ch);
-                    val = hm.get(ch);
-                    val--;
-                    if (val == 0) {
-                        hm.remove(ch);
-                    } else {
-                        hm.put(ch, val);
-                    }
-                }
+            for (int i = 0; i < 26; i++) {
+                if (arr[i] > 0) {
+                    sb.append((char)('a' + i));
+                    arr[i]--;
+                } 
             }
-            for (ch = 'z'; ch >= 'a'; ch--) {
-                if (hm.containsKey(ch)) {
-                    sb.append(ch);
-                    val = hm.get(ch);
-                    val--;
-                    if (val == 0) {
-                        hm.remove(ch);
-                    } else {
-                        hm.put(ch, val);
-                    }
-                }
+            for (int i = 25; i >= 0; i--) {
+                if (arr[i] > 0) {
+                    sb.append((char)('a' + i));
+                    arr[i]--;
+                } 
             }
         }
-
 
         return sb.toString();
     }
