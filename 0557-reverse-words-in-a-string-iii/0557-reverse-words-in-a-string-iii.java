15836b0957a7b6662d4a4811e9f6daaae675fe66
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-09-30 19:46:50 -0700

    Time: 4 ms (87.56%), Space: 44.4 MB (39.62%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -15,9 +15,8 @@ class Solution {
     
     public String reverse(String s) {
         StringBuilder sb = new StringBuilder();
-        for (int i = s.length() - 1; i >= 0; i--) {
-            sb.append(s.charAt(i));
-        }
+        sb.append(s);
+        sb.reverse();
         return sb.toString();
     }
 }
