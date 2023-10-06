6b80a2a0eeb1cec7ddb00b90d46ae384b1a62842
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-10-05 22:04:22 -0700

    Time: 3 ms (78.77%), Space: 44.1 MB (35.13%) - LeetHub

--- 2032-two-out-of-three/2032-two-out-of-three.java
+++ 2032-two-out-of-three/2032-two-out-of-three.java
@@ -1,27 +1,25 @@
 class Solution {
     public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
-        HashSet<Integer> set = new HashSet<>();
-        HashSet<Integer> set1 = new HashSet<>();
-        HashSet<Integer> set2 = new HashSet<>();
-        HashSet<Integer> set3 = new HashSet<>();
+        boolean[] cnt = new boolean[101];
+        Set<Integer> set = new HashSet<>(); 
+        Set<Integer> set2 = new HashSet<>();
         
         for (int num : nums1) {
-            set1.add(num);
+            cnt[num] = true;
         }
         for (int num : nums2) {
             set2.add(num);
         }
-        for (int num : nums3) {
-            set3.add(num);
-        }
-        
-        for (int num : set1) {
-            if (set2.contains(num) || set3.contains(num)) {
+        for (int num : set2) {
+            if (cnt[num]) { 
                 set.add(num);
+            } else {
+                cnt[num] = true;
             }
         }
-        for (int num : set2) {
-            if (set1.contains(num) || set3.contains(num)) {
+        
+        for (int num : nums3) {
+            if (cnt[num]) { 
                 set.add(num);
             }
         }
