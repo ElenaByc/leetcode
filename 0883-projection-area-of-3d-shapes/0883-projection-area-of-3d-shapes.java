0fbf40aecec208328aec9453bcec7a64201daa65
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-10-17 21:06:47 -0700

    Time: 2 ms (87.24%), Space: 42.6 MB (91.38%) - LeetHub

--- 0883-projection-area-of-3d-shapes/0883-projection-area-of-3d-shapes.java
+++ 0883-projection-area-of-3d-shapes/0883-projection-area-of-3d-shapes.java
@@ -1,14 +1,9 @@
 class Solution {
     public int projectionArea(int[][] grid) {
-        // s1(bottom) = number of non zero elements in grid
-        // s2 - sum of max row
-        // s3 - max of max column
         int n = grid.length;
-        int s1 = 0;
-        int s2 = 0;
-        int s3 = 0;
         int maxInColumn;
         int maxInRow;
+        int area = 0;
         
         for (int i = 0; i < n; i++) {
             maxInColumn = 0;
@@ -17,13 +12,13 @@ class Solution {
                 maxInColumn = Math.max(maxInColumn, grid[j][i]);
                 maxInRow = Math.max(maxInRow, grid[i][j]);
                 if (grid[i][j] != 0) {
-                    s1++;
+                    area++;
                 }
             }
-            s2 += maxInRow;
-            s3 += maxInColumn; 
+            area += maxInRow;
+            area += maxInColumn; 
         }
 
-        return s1 + s2 + s3;
+        return area;
     }
 }
