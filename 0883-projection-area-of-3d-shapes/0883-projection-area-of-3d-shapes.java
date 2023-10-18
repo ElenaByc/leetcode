7081611b744b16e62834882dc787aa87686f3074
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-10-17 21:03:38 -0700

    Time: 2 ms (87.24%), Space: 43 MB (49.66%) - LeetHub

--- 0883-projection-area-of-3d-shapes/0883-projection-area-of-3d-shapes.java
+++ 0883-projection-area-of-3d-shapes/0883-projection-area-of-3d-shapes.java
@@ -4,32 +4,26 @@ class Solution {
         // s2 - sum of max row
         // s3 - max of max column
         int n = grid.length;
-        int maxInRow;
-        int s2 = 0;
         int s1 = 0;
-        for (int[] row : grid) {
+        int s2 = 0;
+        int s3 = 0;
+        int maxInColumn;
+        int maxInRow;
+        
+        for (int i = 0; i < n; i++) {
+            maxInColumn = 0;
             maxInRow = 0;
-            for (int num : row) {
-                maxInRow = Math.max(maxInRow, num);
-                if (num != 0) {
+            for (int j = 0; j < n; j++) {
+                maxInColumn = Math.max(maxInColumn, grid[j][i]);
+                maxInRow = Math.max(maxInRow, grid[i][j]);
+                if (grid[i][j] != 0) {
                     s1++;
                 }
             }
             s2 += maxInRow;
+            s3 += maxInColumn; 
         }
-        
-        int s3 = 0;
-        int maxInColumn;
-        for (int i = 0; i < n; i++) {
-            maxInColumn = 0;
-            for (int j = 0; j < n; j++) {
-                maxInColumn = Math.max(maxInColumn, grid[j][i]);
-            }
-            s3 += maxInColumn;
-        }
-        
-        
-        
+
         return s1 + s2 + s3;
     }
 }
