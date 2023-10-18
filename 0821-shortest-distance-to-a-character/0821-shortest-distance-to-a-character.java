ad524746e956ca7f12bb345e69473119f73bec27
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-10-18 10:28:01 -0700

    Time: 1 ms (98.81%), Space: 41.3 MB (87.94%) - LeetHub

--- 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.java
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.java
@@ -2,50 +2,34 @@ class Solution {
     public int[] shortestToChar(String s, char c) {
         int n = s.length();
         int[] answer = new int[n];
-        int leftDist, rightDist;
-        
-        for (int i = 0; i < n; i++) {
-            if (s.charAt(i) == c) {
-                answer[i] = 0;
-            } else {
-                leftDist = getLeftDist(s, i, c);
-                rightDist = getRightDist(s, i, c);
-                answer[i] = Math.min(leftDist, rightDist);
+        int prev, next;
+        // Find the first occurrence of c
+        next = findNext(s, c, 0);
+        for (int i = next; i >= 0; i--) {
+            answer[i] = next - i;
+        }
+        while (next < n) {
+            prev = next;
+            next = findNext(s, c, next + 1);
+            for (int i = prev + 1; i <= next && i < n; i++) {
+                answer[i] = Math.min (i - prev, next - i);
             }
         }
         
         return answer;
+        
     }
     
-    public int getLeftDist(String s, int pos, char c) {
-        int n =  s.length();
-        if (pos == 0) return n;
-        int i = pos - 1;
-        int dist = 1;
+    public int findNext(String s, char c, int startPos) {
+        int next = startPos;
+        int n = s.length();
         
-        while (i >= 0 && s.charAt(i) != c) {
-            i--;
-            dist++;
+        while (next < n && s.charAt(next) != c) {
+            next++;
         }
-        if (i < 0 && s.charAt(0) != c) {
-            return n;
+        if (next == n && s.charAt(n - 1) != c) {
+            return Integer.MAX_VALUE;
         }
-        return dist;
-    }
-
-    public int getRightDist(String s, int pos, char c) {
-        int n =  s.length();
-        if (pos == n - 1) return n;
-        int i = pos + 1;
-        int dist = 1;
-        while (i < n && s.charAt(i) != c) {
-            i++;
-            dist++;
-        }
-        if (i == n && s.charAt(n- 1) != c) {
-            return n;
-        }
-        
-        return dist;
+        return next;
     }
 }
