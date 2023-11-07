be24ab09182344317a87d870319db82c0a56c6db
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-11-06 19:52:56 -0800

    Time: 6 ms (90.14%), Space: 42.5 MB (98.82%) - LeetHub

--- 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.java
+++ 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.java
@@ -2,21 +2,25 @@ class Solution {
     public int similarPairs(String[] words) {
         int cnt = 0;
         int n = words.length;
-        List<HashSet<Character>> list = new ArrayList<>();
-        HashSet<Character> set;
+        boolean[][] grid = new boolean[n][26];
+        boolean equal;
         
-        for (String word: words) {
-            set = new HashSet<>();
-            for (int j = 0; j < word.length(); j++) {
-                set.add(word.charAt(j));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < words[i].length(); j++) {
+                grid[i][words[i].charAt(j) - 'a'] = true;
             }
-            list.add(set);
         }
         
         for (int i = 0; i < n; i++) {
-            set = list.get(i);
             for (int j = i + 1; j < n; j++) {
-                if (set.equals(list.get(j))) {
+                equal = true;
+                for (int k = 0; k < 26; k++) {
+                    if (grid[i][k] != grid[j][k]) {
+                        equal = false;
+                        break;
+                    }
+                }
+                if (equal) {
                     cnt++;
                 }
             }
