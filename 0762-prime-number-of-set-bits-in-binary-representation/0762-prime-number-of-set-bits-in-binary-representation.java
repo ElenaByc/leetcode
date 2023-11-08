1eef414efde8676cb742796cdf6e2eae7e167d2d
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-11-07 21:42:52 -0800

    Time: 4 ms (94.96%), Space: 39.2 MB (67.44%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.java
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.java
@@ -1,24 +1,30 @@
 class Solution {
     public int countPrimeSetBits(int left, int right) {
-        Set<Integer> primes = new HashSet<>();
-        primes.add(2);
-        primes.add(3);
-        primes.add(5);
-        primes.add(7);
-        primes.add(11);
-        primes.add(13);
-        primes.add(17);
-        primes.add(19);
+        // Set<Integer> primes = new HashSet<>();
+        // primes.add(2);
+        // primes.add(3);
+        // primes.add(5);
+        // primes.add(7);
+        // primes.add(11);
+        // primes.add(13);
+        // primes.add(17);
+        // primes.add(19);
         
         int result = 0;
         
         for (int i = left; i <= right; i++) {
-            if (primes.contains(Integer.bitCount(i))) {
+            // if (primes.contains(Integer.bitCount(i))) {
+            if (isSmallPrime(Integer.bitCount(i))) {
                 result++;
             }
         }
         
         return result;
+    }
+    
+    public boolean isSmallPrime(int x) {
+        return (x == 2 || x == 3 || x == 5 || x == 7 ||
+                x == 11 || x == 13 || x == 17 || x == 19);
     }
     
     public int countOnes(int num) {
