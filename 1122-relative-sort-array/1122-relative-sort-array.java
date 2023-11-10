0d9625834a70d2bf29ca20f23814ab01aac7e195
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-11-09 16:56:05 -0800

    Time: 0 ms (100.00%), Space: 41.1 MB (56.05%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+//         HashMap<Integer, Integer> hm = new HashMap<>();
+        
+//         for (int i = 0; i < arr2.length; i++){
+//             hm.put(arr[i], i);
+//         }   
+        
+//         int n = arr1.length;
+//         int[] result = new int[n];
+        
+//         for (int i = 0; i < n; i++) {
+            
+//         }
+        
+        int[] count = new int[1001]; // since numbers are from [0, 1000]
+
+        for (int num : arr1) {
+            count[num]++;
+        }
+
+        int i = 0;
+        for (int num : arr2) {
+            while (count[num] > 0) {
+                arr1[i] = num;
+                i++;
+                count[num]--;
+            }
+        }
+
+        // For remaining elements that were not in arr2
+        for (int j = 0; j < 1001; j++) {
+            while (count[j] > 0) {
+                arr1[i] = j;
+                i++;
+                count[j]--;
+            }
+        }
+
+        return arr1;
+    }
+}
