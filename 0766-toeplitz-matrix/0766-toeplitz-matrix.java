33e7c0eaf5f2b7c570648bfa5dce3771b7545bd4
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-11-10 17:39:28 -0800

    Time: 0 ms (100.00%), Space: 43.1 MB (55.33%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.java
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -1,10 +1,7 @@
 class Solution {
     public boolean isToeplitzMatrix(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
                 if (matrix[i][j] != matrix[i-1][j-1]) {
                     return false;
                 }
