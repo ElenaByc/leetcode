f350ba56f35e5b3ae1299c13d1616b95247fc7fd
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-11-12 18:50:01 -0800

    Time: 42 ms (75.00%), Space: 45.7 MB (61.42%) - LeetHub

---
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String sortVowels(String s) {
+        int n = s.length();
+        List<Character> vowels = new ArrayList<>();
+        char[] result = new char[n];
+        char ch;
+        for (int i = 0; i < n; i++) {
+            ch = s.charAt(i);
+            if (isVowel(ch)) {
+                vowels.add(ch);
+                result[i] = 'a';
+            } else {
+                result[i] = ch;
+            }
+        }
+        
+        Collections.sort(vowels);
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            if (result[i] == 'a') {
+                result[i] = vowels.get(j);
+                j++;
+            }
+        }
+        
+        return new String(result);
+    }
+    
+    public boolean isVowel(char ch) {
+        ch = Character.toLowerCase(ch);
+        return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
+    }
+}
