8ff5efea1e1c2034b7b49f102e7774e69841eab6
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-11-12 19:33:59 -0800

    Time: 9 ms (99.40%), Space: 43.5 MB (100.00%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.java
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.java
@@ -1,25 +1,67 @@
 class Solution {
     public String sortVowels(String s) {
         int n = s.length();
-        List<Character> vowels = new ArrayList<>();
+        int[] vowels = new int[10];
         char[] result = new char[n];
+        String sortedVowel = "AEIOUaeiou";
         char ch;
         for (int i = 0; i < n; i++) {
             ch = s.charAt(i);
             if (isVowel(ch)) {
-                vowels.add(ch);
+                switch (ch) {
+                    case 'A':
+                        vowels[0]++;
+                        break;
+                    case 'E':
+                        vowels[1]++;
+                        break;
+                    case 'I':
+                        vowels[2]++;
+                        break;
+                    case 'O':
+                        vowels[3]++;
+                        break;
+                    case 'U':
+                        vowels[4]++;
+                        break;
+                    case 'a':
+                        vowels[5]++;
+                        break;
+                    case 'e':
+                        vowels[6]++;
+                        break;
+                    case 'i':
+                        vowels[7]++;
+                        break;
+                    case 'o':
+                        vowels[8]++;
+                        break;
+                    case 'u':
+                        vowels[9]++;
+                        break;
+                }
                 result[i] = 'a';
             } else {
                 result[i] = ch;
             }
         }
         
-        Collections.sort(vowels);
         int j = 0;
+        while (j < 10 && vowels[j] == 0) {
+            j++;
+        }
+        if (j == 10) {
+             return new String(result);
+        }
+        ch = sortedVowel.charAt(j);
         for (int i = 0; i < n; i++) {
             if (result[i] == 'a') {
-                result[i] = vowels.get(j);
-                j++;
+                while (vowels[j] == 0) {
+                    j++;
+                }
+                ch = sortedVowel.charAt(j);
+                result[i] = ch;
+                vowels[j]--;
             }
         }
         
