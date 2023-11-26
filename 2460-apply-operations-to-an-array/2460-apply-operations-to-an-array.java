e9f6b85b9b69ddfcbb825d6cb2fc3f1a4a60eaf2
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-11-25 21:26:55 -0800

    Time: 1 ms (89.25%), Space: 43.4 MB (76.32%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
@@ -1,7 +1,6 @@
 class Solution {
     public int[] applyOperations(int[] nums) {
         int n = nums.length;
-        int[] result = new int[n];
         
         for (int i = 0; i < n - 1; i++) {
             if (nums[i] == nums[i + 1]) {
@@ -10,15 +9,23 @@ class Solution {
             }
         }
         
-        int i = 0;
-        for (int num : nums) {
-            if (num != 0) {
-                result[i] = num;
-                i++;
+        int p1 = 0;
+        int p2 = 0;
+        while (p2 < n) {
+            while (p2 < n && nums[p2] == 0) {
+                p2++;
+            }
+            if (p2 < n) {
+                nums[p1] = nums[p2];
+                p1++;
+                p2++;
             }
         }
-        
-        return result;
-        
+        while (p1 < n) {
+            nums[p1] = 0;
+            p1++;
+        }
+
+        return nums;
     }
 }
