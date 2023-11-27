4a80d873beea125a1b1f59dc61904cd08d150292
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-11-26 20:32:55 -0800

    Time: 9 ms (62.36%), Space: 44.5 MB (14.04%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
@@ -1,15 +1,6 @@
 class Solution {
     public int countCharacters(String[] words, String chars) {
-        int sum = 0;    
-        for (String word : words) {
-            if (canBeFormed(word, chars)) {
-                sum += word.length();
-            } 
-        }
-        return sum;
-    }
-    
-    public boolean canBeFormed(String word, String chars) {
+        int sum = 0;
         HashMap<Character, Integer> hm = new HashMap<>();
         char ch;
         
@@ -18,11 +9,23 @@ class Solution {
             hm.put(ch, hm.getOrDefault(ch, 0) + 1);
         }
         
+        for (String word : words) {
+            if (canBeFormed(word, hm)) {
+                sum += word.length();
+            } 
+        }
+        return sum;
+    }
+    
+    public boolean canBeFormed(String word, HashMap<Character, Integer> hm) {
+        HashMap<Character, Integer> hm2 = new HashMap<>();
+        char ch;
+        
         for (int i = 0; i < word.length(); i++) {
             ch = word.charAt(i);
             if (!hm.containsKey(ch)) return false;
-            if (hm.get(ch) <= 0) return false;
-            hm.put(ch, hm.get(ch) - 1);
+            hm2.put(ch, hm2.getOrDefault(ch, 0) + 1);
+            if (hm2.get(ch) > hm.get(ch)) return false;
         }
         
         return true;
