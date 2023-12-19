40affb375122c07317670b6c27ad7656d09bf1e5
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-12-18 19:45:39 -0800

    Time: 265 ms (6.16%), Space: 44.9 MB (14.85%) - LeetHub

---
+++ 0661-image-smoother/0661-image-smoother.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int[][] imageSmoother(int[][] img) {
+        int m = img.length;
+        int n = img[0].length;
+        int[][] result  = new int[m][n];
+        int sum;
+        int cnt;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                sum = 0;
+                cnt = 0;
+                // current row
+                if (j > 0) {
+                    sum += img[i][j - 1];
+                    cnt++;
+                }
+                sum += img[i][j];
+                cnt++;
+                if (j < n - 1) {
+                    sum += img[i][j + 1];
+                    cnt++;
+                }
+                // prev row
+                if (i > 0) {
+                    if (j > 0) {
+                        sum += img[i - 1][j - 1];
+                        cnt++;
+                    }
+                    sum += img[i - 1][j];
+                    cnt++;
+                    if (j < n - 1) {
+                        sum += img[i - 1][j + 1];
+                        cnt++;
+                    }
+                }
+                // next row
+                if (i < m - 1) {
+                    if (j > 0) {
+                        sum += img[i + 1][j - 1];
+                        cnt++;
+                    }
+                    sum += img[i + 1][j];
+                    cnt++;
+                    if (j < n - 1) {
+                        sum += img[i + 1][j + 1];
+                        cnt++;
+                    }
+                }
+                System.out.println("sum = " + sum + "     cnt = " + cnt);
+                result[i][j] = sum / cnt;
+            }
+        }
+        
+        return result;
+    }
+}
