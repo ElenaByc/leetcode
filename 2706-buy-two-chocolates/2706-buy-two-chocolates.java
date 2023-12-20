e56ce91d97b6f650d98ce8cea584e32d43929b55
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-12-19 16:41:25 -0800

    Time: 16 ms (5.97%), Space: 43.9 MB (10.63%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        int n = prices.length;
+        int min1 = 101;
+        int min1Idx = -1;
+        int min2 = 101;
+        
+        for (int i = 0; i < n; i++) {
+            if (prices[i] < min1) {
+                min1 = prices[i];
+                min1Idx = i;
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (prices[i] < min2 && i != min1Idx) {
+                min2 = prices[i];
+            }
+        }
+        
+        System.out.println("min1 = " + min1);
+        System.out.println("min2 = " + min2);
+        
+        if (min1 + min2 <= money) {
+            return money - min1 - min2;
+        }
+        
+        return  money;
+    }
+}
