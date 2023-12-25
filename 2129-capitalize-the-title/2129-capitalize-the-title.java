9bfca7e4f4f830f28e2615017d3ad57eb459f83c
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-12-24 23:02:32 -0800

    Time: 4 ms (68.87%), Space: 42.6 MB (27.04%) - LeetHub

---
+++ 2129-capitalize-the-title/2129-capitalize-the-title.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String capitalizeTitle(String title) {
+        String[] words = title.split(" ");
+        
+        for (int i = 0; i < words.length; i++) {
+            words[i] = capitalize(words[i]);
+        }
+        return String.join(" ", words);
+    }
+    
+    public String capitalize(String word) {
+        int n = word.length();
+        if (n < 3) {
+            return word.toLowerCase();
+        }
+        
+        return word.substring(0,1).toUpperCase() + word.substring(1).toLowerCase();
+    }
+}
