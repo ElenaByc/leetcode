15ea2bcd15d102f751d97933555ff476c01da4f3
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-12-24 23:06:03 -0800

    Time: 5 ms (46.23%), Space: 42.4 MB (31.13%) - LeetHub

--- 2129-capitalize-the-title/2129-capitalize-the-title.java
+++ 2129-capitalize-the-title/2129-capitalize-the-title.java
@@ -1,18 +1,16 @@
 class Solution {
     public String capitalizeTitle(String title) {
-        String[] words = title.split(" ");
-        
-        for (int i = 0; i < words.length; i++) {
-            words[i] = capitalize(words[i]);
+        StringBuilder sb = new StringBuilder();
+        String[] arr = title.split(" ");
+        for (String s : arr)
+        {
+            if (s.length() <= 2 ) {
+                sb.append(s.toLowerCase() + " ");
+            }else {
+                sb.append(s.substring(0, 1).toUpperCase());
+                sb.append(s.substring(1).toLowerCase() + " ");
+            }
         }
-        return String.join(" ", words);
-    }
-    
-    public String capitalize(String word) {
-        int n = word.length();
-        if (n < 3) {
-            return word.toLowerCase();
-        }
-        return word.substring(0,1).toUpperCase() + word.substring(1).toLowerCase();
+        return sb.toString().trim();
     }
 }
