590461630545f3e1dd19702fba1e464f6370e011
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-01-04 17:24:23 -0800

    Time: 2 ms (96.15%), Space: 44.6 MB (10.31%) - LeetHub

--- 2164-sort-even-and-odd-indices-independently/2164-sort-even-and-odd-indices-independently.java
+++ 2164-sort-even-and-odd-indices-independently/2164-sort-even-and-odd-indices-independently.java
@@ -1,37 +1,29 @@
 class Solution {
     public int[] sortEvenOdd(int[] nums) {
         int n = nums.length;
-        int[] result = new int[n];
-        
-        List<Integer> odds = new ArrayList<>();
-        List<Integer> evens = new ArrayList<>();
-        
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) {
-                evens.add(nums[i]);
-            } else {
-                odds.add(nums[i]);
+        int[] even = new int[(n+1)/2];
+        int[] odd = new int[n/2];
+        int iE=0;
+        int iO =0;
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                even[iE++] = nums[i];
+            }else{
+                odd[iO++] = nums[i];
             }
         }
-        
-        evens.sort(Comparator.naturalOrder());
-        odds.sort(Comparator.reverseOrder());
-        
-        System.out.println(odds);
-        System.out.println(evens);
-        int j = 0;
-        int k = 0;
-        
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) {
-                result[i] = evens.get(j);
-                j++;
-            } else {
-                result[i] = odds.get(k);
-                k++;
+        Arrays.sort(even);
+        Arrays.sort(odd);
+        iE =0;
+        iO =n/2 -1;
+
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                nums[i] = even[iE++];
+            }else{
+                nums[i] = odd[iO--];
             }
         }
-        
-        return result;
+        return nums;
     }
 }
