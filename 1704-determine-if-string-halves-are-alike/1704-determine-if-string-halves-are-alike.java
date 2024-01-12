cc4e21231771949d6857f688c1622afc2327a6e6
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-01-11 21:09:57 -0800

    Time: 3 ms (88.24%), Space: 41.4 MB (43.26%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,22 +1,21 @@
 class Solution {
     public boolean halvesAreAlike(String s) {
-        s = s.toLowerCase();
-        int counter1 = 0;
-        int counter2 = 0;
-        int n = s.length();
-        char ch;
-        
-        for (int i = 0; i < n /2; i++) {
-            ch = s.charAt(i);
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
-                counter1++;
-            }
-            ch = s.charAt(n - 1 - i);
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
-                counter2++;
+        int vowels = 0;
+        int n =  s.length();
+        for (int i = 0; i < n; i++) {
+            if (isVowel(s.charAt(i))) {
+                if (i < n / 2) {
+                    vowels++;
+                } else {
+                    vowels--;
+                }
             }
         }
-        
-        return counter1 == counter2;
+                        
+        return vowels == 0;
+    }
+                        
+    public static boolean isVowel (char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch ==  'o' || ch ==  'u' || ch ==  'A' || ch ==  'E' || ch ==  'I' || ch ==  'O' || ch ==  'U';
     }
 }
