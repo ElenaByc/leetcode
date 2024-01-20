32087df7dcdf85dfa0efc3d40abb37f65aabbbd0
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-01-19 19:52:48 -0800

    Time: 5 ms (37.21%), Space: 42.1 MB (8.71%) - LeetHub

--- 1507-reformat-date/1507-reformat-date.java
+++ 1507-reformat-date/1507-reformat-date.java
@@ -1,25 +1,27 @@
 class Solution {
     public String reformatDate(String date) {
-        String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+        HashMap<String, String> hm = new HashMap<>();
+        hm.put("Jan", "01");
+        hm.put("Feb", "02");
+        hm.put("Mar", "03");
+        hm.put("Apr", "04");
+        hm.put("May", "05");
+        hm.put("Jun", "06");
+        hm.put("Jul", "07");
+        hm.put("Aug", "08");
+        hm.put("Sep", "09");
+        hm.put("Oct", "10");
+        hm.put("Nov", "11");
+        hm.put("Dec", "12");
+        
         int n = date.length();
-        int month = 0;
-        String monthStr = date.substring(n-8, n-5);
-        for (int i = 0; i < 12; i++) {
-            if (months[i].equals(monthStr)) {
-                month = i + 1;
-                break;
-            }
-        }
-        if (month < 10) {
-            monthStr = "0" + Integer.toString(month);
+        String dateStr;
+        if (n == 13) {
+            dateStr = date.substring(0, 2);
         } else {
-            monthStr = Integer.toString(month);
-        }
-        String dateStr = date.substring(0, n - 11);
-        if (dateStr.length() == 1) {
-            dateStr = "0" + dateStr;
+             dateStr = "0" + date.substring(0, 1);
         }
         
-        return date.substring(n - 4) + "-" + monthStr + "-" + dateStr;
+        return date.substring(n - 4) + "-" + hm.get(date.substring(n-8, n-5)) + "-" + dateStr;
     }
 }
