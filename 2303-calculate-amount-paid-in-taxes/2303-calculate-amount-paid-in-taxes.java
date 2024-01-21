4fcf1d17dab9849f8b375e64b0c29abca9794b40
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-01-20 19:49:53 -0800

    Time: 0 ms (100.00%), Space: 44.5 MB (40.36%) - LeetHub

--- 2303-calculate-amount-paid-in-taxes/2303-calculate-amount-paid-in-taxes.java
+++ 2303-calculate-amount-paid-in-taxes/2303-calculate-amount-paid-in-taxes.java
@@ -1,24 +1,19 @@
 class Solution {
     public double calculateTax(int[][] brackets, int income) {
-        double tax = 0;
+
+        if (income <= brackets[0][0]) {
+            return income * brackets[0][1] / 100.0;
+        }
         int n = brackets.length;
+        double tax =  brackets[0][0] * brackets[0][1] / 100.0;
         
-        for (int i = 0; i < n; i++) {
-            if (income >= brackets[i][0]) {
-                if (i > 0) {
-                    tax += (brackets[i][0] - brackets[i - 1][0]) * brackets[i][1] / 100.0;
-                } else {
-                    tax += (brackets[i][0]) * brackets[i][1] / 100.0;
-                }
-            } else { // income < brackets[i][0]
-                if (i > 0) {
-                    tax += (income - brackets[i - 1][0]) * brackets[i][1] / 100.0;
-                } else {
-                    tax += income * brackets[i][1] / 100.0;
-                }
+        for (int i = 1; i < n; i++) {
+            if (income > brackets[i][0]) {
+                tax += (brackets[i][0] - brackets[i - 1][0]) * brackets[i][1] / 100.0;
+            } else { // income <= brackets[i][0]
+                tax += (income - brackets[i - 1][0]) * brackets[i][1] / 100.0;
                 break;
-            }
-            
+            }   
         }
         
         return tax;
