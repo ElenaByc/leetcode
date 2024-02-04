2ac81defc0ce5d3214a773f1d9ad4f4156fa5692
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-02-03 17:06:25 -0800

    Time: 4 ms (74.60%), Space: 55.4 MB (53.52%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.java
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.java
@@ -4,15 +4,9 @@ class Solution {
             return new int[][] {};
          }
         int[][] result = new int[m][n];
-        int idx = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (idx < original.length) {
-                    result[i][j] = original[idx];
-                    idx++;
-                } else {
-                    break;
-                }
+                result[i][j] = original[i * n + j];           
             }
         }
         
