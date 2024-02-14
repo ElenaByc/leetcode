9dc546dd294673dfa7f9b4e2acb73a11e713f189
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-02-13 23:18:19 -0800

    Time: 2 ms (35.62%), Space: 41.4 MB (81.87%) - LeetHub

--- 1854-maximum-population-year/1854-maximum-population-year.java
+++ 1854-maximum-population-year/1854-maximum-population-year.java
@@ -1,21 +1,22 @@
 class Solution {
     public int maximumPopulation(int[][] logs) {
-        // from 1950 to 2049
-        int[] years = new int[100];
-        
-        for (int[] pair : logs) {
-            for (int i = pair[0]; i < pair[1]; i++) {
-                years[i - 1950]++;
-            }
-        }
-        
-        // System.out.println(Arrays.toString(years));
         int max = -1;
         int maxYear = 1950;
-        for (int i = 0; i < 100; i++) {
-            if (years[i] > max) {
-                max = years[i];
-                maxYear = 1950 + i;
+        int curr = 0;
+
+        for(int i = 1950; i < 2050; i++) {
+            curr = 0;
+            for(int[] pair : logs) {
+                if (pair[0] <= i) {
+                    curr++;
+                }
+                if (pair[1] <= i) {
+                   curr--;
+                }
+            }
+            if (curr > max) {
+                max = curr;
+                maxYear = i;
             }
         }
          
