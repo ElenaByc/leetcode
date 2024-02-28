b34553ffc92183b23b44ed205bb25d8a7a2f1c1c
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-02-27 20:56:25 -0800

    Time: 0 ms (100.00%), Space: 43.1 MB (83.03%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
@@ -14,20 +14,25 @@
  * }
  */
 class Solution {
-   public int findBottomLeftValue(TreeNode root) {
-        Queue<TreeNode> queue = new LinkedList<>();
-        TreeNode current = root;
-        queue.add(current);
+    private int maxDepth;
+    private int bottomLeftValue;
 
-        while (!queue.isEmpty()) {
-            current = queue.poll();
-            if (current.right != null) {
-                queue.add(current.right);
-            }
-            if (current.left != null) {
-                queue.add(current.left);
-            }
+    public int findBottomLeftValue(TreeNode root) {
+        maxDepth = -1;
+        bottomLeftValue = 0;
+        dfs(root, 0);
+        return bottomLeftValue;
+    }
+
+    private void dfs(TreeNode current, int depth) {
+        if (current == null) {
+            return;
         }
-        return current.val;
+        if (depth > maxDepth) {  // If true, we discovered a new level
+            maxDepth = depth;
+            bottomLeftValue = current.val;
+        }
+        dfs(current.left, depth + 1);
+        dfs(current.right, depth + 1);
     }
 }
