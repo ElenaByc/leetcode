bbc9821eca580fd620bd5f0da17775284c0fd082
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-02-29 19:37:05 -0800

    Time: 1 ms (98.75%), Space: 41.1 MB (90.71%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,29 +1,15 @@
 class Solution {
     public boolean isValid(String str) {
-        HashMap<Character, Character> close = new HashMap<>();
-        close.put(')', '(');
-        close.put('}', '{');
-        close.put(']', '[');
-        
-        Set<Character> open = new HashSet<>();
-        open.add('(');
-        open.add('{');
-        open.add('[');
-        
-        Stack<Character> stack = new Stack<>();
-        char ch;
-
-        for (int i = 0; i < str.length(); i++) {
-            ch = str.charAt(i);
-            if (open.contains(ch)) {
-                stack.add(ch);
-            } else if (close.containsKey(ch)) {
-                if (stack.isEmpty()) {
-                    return false;
-                } else if (close.get(ch) != stack.pop()) {
-                    return false;
-                }
-            }
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : str.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
         }
         return stack.isEmpty();
     }
