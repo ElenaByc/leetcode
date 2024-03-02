e63b73fbcb1f41a9407645e94f9a3534cd4e5c83
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-03-01 16:33:44 -0800

    Time: 1 ms (100.00%), Space: 47.5 MB (10.49%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -4,42 +4,46 @@ class Solution {
         if (n == 1) {
             return new int[]{nums[0] * nums[0]};
         }
-        List<Integer> positive0 = new ArrayList<>();
-        List<Integer> negative = new ArrayList<>();
-        
-        for (int num : nums) {
-            if (num < 0) {
-                negative.add(num * num);
-            } else {
-                positive0.add(num * num);
-            }
-        }
-        // merge two sorted lists
         int[] result = new int[n];
-        int n1 = negative.size();
-        int n2 = positive0.size();
-        int i1 = n1 - 1;
-        int i2 = 0;
+        int posIdx = n; // index of first non negative number
+        for (int i = 0; i < n; i++) {
+            if (posIdx == n && nums[i] >=0) {
+                posIdx = i;
+            } 
+            nums[i] *= nums[i];
+        }
+        if (posIdx == 0) { // all numbers >= 0
+            return nums;
+        }
+        if (posIdx == n) { // all numbers negative
+            for (int i = 0; i < n; i++) {
+                result[n - i - 1] = nums[i];
+            }
+            return result;
+        } 
+        
+        // merge two sorted parts
+        int negIdx = posIdx - 1;
         int i = 0;
-        while (i1 >= 0 && i2 < n2) {
-            if (positive0.get(i2) <= negative.get(i1)) {
-                result[i] = positive0.get(i2);
-                i2++;
+        while (negIdx >= 0 && posIdx < n) {
+            if (nums[negIdx] < nums[posIdx]) {
+                result[i] = nums[negIdx];
+                negIdx--;
             } else {
-                result[i] = negative.get(i1);
-                i1--;
+                result[i] = nums[posIdx];
+                posIdx++;
             }
             i++;
         }
-        while (i1 >= 0) {
-            result[i] = negative.get(i1);
-            i1--;
+        while (negIdx >= 0) {
+            result[i] = nums[negIdx];
+            negIdx--;
             i++;
         }
         
-        while (i2 < n2) {
-            result[i] = positive0.get(i2);
-            i2++;
+        while (posIdx < n) {
+            result[i] = nums[posIdx];
+            posIdx++;
             i++;
         }
         
