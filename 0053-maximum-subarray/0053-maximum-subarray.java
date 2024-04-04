d68695d48d2c3152bd371b9bc2d5b104dcc37623
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-04-04 12:50:38 -0700

    Time: 2 ms (7.55%), Space: 56.8 MB (77.91%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -4,24 +4,14 @@ class Solution {
         
         int maxSum = nums[0];
         int sum = 0;
-        
-        int firstPositive = 0;
-        
-        while (firstPositive < n && nums[firstPositive] <= 0) {
-            maxSum = Math.max(maxSum, nums[firstPositive]);
-            firstPositive++;      
-        }
-        if (firstPositive == n) {
-            return maxSum;
-        }
-        
-        for (int i = firstPositive; i < n ; i++) {
+
+        for (int i = 0; i < n ; i++) {
             sum += nums[i];
-  
+            maxSum = Math.max(maxSum, sum);
+
             if (sum < 0) {
                 sum = 0;
             }
-            maxSum = Math.max(maxSum, sum);
         }
         
         return maxSum;
