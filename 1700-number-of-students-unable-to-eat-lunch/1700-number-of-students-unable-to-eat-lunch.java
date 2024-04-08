020689c3d89b8335085d0825cd6eb62cc2444c97
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-04-07 20:28:09 -0700

    Time: 0 ms (100.00%), Space: 41.6 MB (29.87%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
@@ -1,38 +1,26 @@
 class Solution {
     public int countStudents(int[] students, int[] sandwiches) {
+        int zeroCnt = 0;
+        int oneCnt = 0;
         int n = students.length;
-        int students0 = 0;
-        int sandwiches0 = 0;
         
-        for(int s : students){
-            if(s == 0) {
-                students0++;
-            } 
-        }    
-        int students1 = n - students0;
-        
-        for(int s:sandwiches){
-            if(s == 0) {
-                sandwiches0++;
-            }
-        } 
-        if(students0 == sandwiches0){
-            return 0;
-        }
-        
-        for(int s : sandwiches) {
-            if(s == 0){
-                if(students0 == 0) {
-                    return students1;
-                }
-                students0--;
+        for (int s : students) {
+            if (s == 0) {
+                zeroCnt++;
             } else {
-                if(students1 == 0) {
-                    return students0;
-                }
-                students1--;
+                oneCnt++;
             }
         }
-        return 0;
-    }  
+        for (int s : sandwiches) {
+            if (s == 0 && zeroCnt > 0) {
+                zeroCnt--;
+            } else if (s == 1 && oneCnt > 0) {
+                oneCnt--;
+            } else {
+                break;
+            }
+        }
+        
+        return  zeroCnt + oneCnt;
+    }
 }
