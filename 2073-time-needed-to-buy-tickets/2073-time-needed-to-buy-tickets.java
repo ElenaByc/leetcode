a270067d1ecccb24161b21cb6f7195134ab46d76
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-04-08 17:58:10 -0700

    Time: 0 ms (100.00%), Space: 40.9 MB (78.87%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -1,19 +1,19 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
+        int num = tickets[k];
         int time = 0;
-        int n = tickets.length;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < tickets.length; i ++) {
             if (i <= k) {
-                if (tickets[i] < tickets[k]) {
-                    time += tickets[i];
+                if (tickets[i] >= num) {
+                    time += num;
                 } else {
-                    time += tickets[k];
+                    time += tickets[i];
                 }
             } else {
-                if (tickets[i] < tickets[k]) {
-                    time += tickets[i];
+                if (tickets[i] >= num - 1) {
+                    time += num - 1;
                 } else {
-                    time += tickets[k] - 1;
+                    time += tickets[i];
                 }
             }
         }
