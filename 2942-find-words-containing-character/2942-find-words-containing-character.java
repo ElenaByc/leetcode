278dbe3f47c502d6e0306c20d7938f29ee802da0
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-04-08 18:46:51 -0700

    Time: 2 ms (61.57%), Space: 45 MB (22.49%) - LeetHub

--- 2942-find-words-containing-character/2942-find-words-containing-character.java
+++ 2942-find-words-containing-character/2942-find-words-containing-character.java
@@ -3,8 +3,11 @@ class Solution {
         var list = new ArrayList<Integer>();
         
         for (int i = 0; i < words.length; i++) {
-            if (words[i].contains("" + x)) {
-                list.add(i);
+            for (int j = 0; j < words[i].length(); j++) {
+                if (words[i].charAt(j) == x) {
+                    list.add(i);
+                    break;
+                }
             }
         }
         
