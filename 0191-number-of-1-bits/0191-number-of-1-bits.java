8454c2adfee07e79b88b866f93029f621c541b30
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-04-10 16:51:30 -0700

    Time: 0 ms (100.00%), Space: 40.9 MB (17.14%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,11 +1,10 @@
-public class Solution {
-    // you need to treat n as an unsigned value
+class Solution {
     public int hammingWeight(int n) {
-        int ans = 0;
-        while (n != 0) {
-            n &= (n - 1);
-            ans++;
+        int cnt = 0;
+        while (n > 0) {
+            cnt += n & 1;
+            n >>= 1;
         }
-        return ans;
+        return cnt;
     }
 }
