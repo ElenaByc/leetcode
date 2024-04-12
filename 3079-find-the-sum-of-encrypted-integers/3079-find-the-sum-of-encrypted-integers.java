7f093afc865e9a35c67d6121bf4f828058ce0e13
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-04-11 17:22:58 -0700

    Time: 1 ms (99.95%), Space: 42.7 MB (76.05%) - LeetHub

--- 3079-find-the-sum-of-encrypted-integers/3079-find-the-sum-of-encrypted-integers.java
+++ 3079-find-the-sum-of-encrypted-integers/3079-find-the-sum-of-encrypted-integers.java
@@ -3,16 +3,22 @@ class Solution {
         int sum = 0;
         int max = 0;
         int len;
+        int mul;
         
         for (int n : nums) {
             max = 0;
             len = 0;
+            mul = 1;
             while (n > 0) {
                 len++;
                 max = Math.max(max, n % 10);
                 n /= 10;
             }
-            sum += Integer.parseInt((Integer.toString(max)).repeat(len));
+            while (len > 0) {
+                sum += max * mul;
+                len--;
+                mul *= 10;
+            }
         }
         
         return sum;
