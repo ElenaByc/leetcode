9fadc52f9580e706d3f73fd21c0405604ec051fd
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-04-20 18:25:25 -0700

    Time: 187 ms (29.76%), Space: 159.9 MB (30.79%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -0,0 +1,60 @@
+class Solution {
+    public boolean validPath(int n, int[][] edges, int s, int d) {
+        if (s == d) {
+            return true;
+        }
+        int[] components = new int[n];
+        var hm = new HashMap<Integer, LinkedList<Integer>>();
+        LinkedList list;
+        
+        for (int[] e : edges) {
+            if (hm.containsKey(e[0])) {
+                list = hm.get(e[0]);
+            } else {
+                list = new LinkedList<>();
+            }
+            list.add(e[1]);
+            hm.put(e[0], list);
+            if (hm.containsKey(e[1])) {
+                list = hm.get(e[1]);
+            } else {
+                list = new LinkedList<>();
+            }
+            list.add(e[0]);
+            hm.put(e[1], list);
+        }
+        if (!hm.containsKey(s) || !hm.containsKey(d)) {
+            return false;
+        }
+        
+        // System.out.println(hm);
+        int v;
+        int componentCnt = 1;
+        for (int i = 0; i < n; i++) {
+            // System.out.println("i = " + i);
+            // System.out.println(Arrays.toString(components));
+            if (components[i] == 0) {
+                components[i] = componentCnt;
+                v = i;
+                if (hm.containsKey(v)) {
+                    list = hm.get(v);
+                    while (!list.isEmpty()) {
+                        v = (int)list.poll();
+                        components[v] = componentCnt;
+                        for (int v1 : hm.get(v)) {
+                            if (components[v1] == 0) {
+                                list.add(v1);
+                            }
+                        }
+                    }
+                }
+                componentCnt++;
+            }
+            // System.out.println(Arrays.toString(components));
+        }
+        
+        return components[s] == components[d];
+    }
+    
+    
+}
