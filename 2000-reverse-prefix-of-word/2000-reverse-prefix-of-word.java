201e28535b9cd54c1987fd8288905fe463c1dfcd
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-04-30 17:25:34 -0700

    Time: 0 ms (100.00%), Space: 41.7 MB (42.61%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
@@ -1,14 +1,10 @@
 class Solution {
     public String reversePrefix(String word, char ch) {
         int i = word.indexOf(ch);
-        if (i == -1) return word;
-        
-        StringBuilder sb = new StringBuilder();
-        
-        sb.append(word.substring(0, i + 1));
-        sb.reverse();
-        sb.append(word.substring (i + 1));
-        
-        return sb.toString();
+        if (i > -1) {
+            return new StringBuilder(word.substring(0, i + 1)).reverse().toString() + word.substring(i + 1);
+        } else {
+            return word;
+        }
     }
 }
