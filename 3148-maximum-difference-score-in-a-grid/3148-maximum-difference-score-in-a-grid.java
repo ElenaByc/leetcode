4abf002655587971364a2fa2f2c060aa7c9a14fd
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-05-11 21:51:01 -0700

    Time: 9 ms (100.00%), Space: 63.3 MB (50.00%) - LeetHub

--- 3148-maximum-difference-score-in-a-grid/3148-maximum-difference-score-in-a-grid.java
+++ 3148-maximum-difference-score-in-a-grid/3148-maximum-difference-score-in-a-grid.java
@@ -2,19 +2,19 @@ class Solution {
     public int maxScore(List<List<Integer>> grid) {
         int m = grid.size();
         int n = grid.get(0).size();
+        int[][] dp = new int[m][n];
         int maxScore = Integer.MIN_VALUE;
 		
         
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
+                dp[i][j] = grid.get(i).get(j);
                 int min = Math.min(
-                    i > 0 ? grid.get(i - 1).get(j) : 1000000,
-                    j > 0 ? grid.get(i).get(j - 1) : 1000000
+                    i > 0 ? dp[i - 1][j] : 1000000,
+                    j > 0 ? dp[i][j - 1] : 1000000
                 );
-                maxScore = Math.max(maxScore, grid.get(i).get(j) - min);
-                if (min < grid.get(i).get(j)) {
-                    grid.get(i).set(j, min);
-                }
+                maxScore = Math.max(maxScore, dp[i][j] - min);
+                dp[i][j] = Math.min(dp[i][j], min);
             }
         }
     
