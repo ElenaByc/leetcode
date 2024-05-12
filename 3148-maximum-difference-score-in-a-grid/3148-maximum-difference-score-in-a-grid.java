f33672f2948c34347b6d3f90c6b99fd32bafb2c0
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-05-11 21:46:01 -0700

    Time: 29 ms (50.00%), Space: 62.7 MB (50.00%) - LeetHub

--- 3148-maximum-difference-score-in-a-grid/3148-maximum-difference-score-in-a-grid.java
+++ 3148-maximum-difference-score-in-a-grid/3148-maximum-difference-score-in-a-grid.java
@@ -3,29 +3,21 @@ class Solution {
         int m = grid.size();
         int n = grid.get(0).size();
         int maxScore = Integer.MIN_VALUE;
-		int[][] dp = new int[m][n];
+		
         
-        for(int i = m - 1; i >= 0; i--){
-            for(int j = n - 1; j >= 0; j--){
-                dp[i][j] = grid.get(i).get(j);
-                if(i < m - 1){
-                    dp[i][j] = Math.max(dp[i][j], dp[i + 1][j]);
-                }
-                if(j < n - 1){
-                    dp[i][j] = Math.max(dp[i][j], dp[i][j + 1]);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int min = Math.min(
+                    i > 0 ? grid.get(i - 1).get(j) : 1000000,
+                    j > 0 ? grid.get(i).get(j - 1) : 1000000
+                );
+                maxScore = Math.max(maxScore, grid.get(i).get(j) - min);
+                if (min < grid.get(i).get(j)) {
+                    grid.get(i).set(j, min);
                 }
             }
         }
-			for(int i = 0; i < m; i++){
-				for(int j = 0; j < n; j++){
-					if(i < m - 1){
-						maxScore = Math.max(maxScore, dp[i + 1][j] - grid.get(i).get(j));
-					}
-					if(j < n - 1){
-						maxScore = Math.max(maxScore, dp[i][j + 1] - grid.get(i).get(j));
-					}
-				}
-			}
+    
         return maxScore;
     }
 }
