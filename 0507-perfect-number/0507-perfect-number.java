985eb20b8aedad5f3b85ddaa6c223d8da4aeeab5
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-05-21 17:27:44 -0700

    Time: 1 ms (93.48%), Space: 40.2 MB (64.23%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,17 +1,14 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        int sum = 0;
+        int sum = num * (-1);
         
-        for (int i = 1; i * i <= num; i++) {
+        for (int i = 1; i * i < num; i++) {
             if (num % i == 0) {
                 sum += i;
-                if (i * i != num) {
-                    sum += num / i;
-                }
-
+                sum += num / i;
             }
         }
         
-        return sum - num == num;
+        return sum == num;
     }
 }
