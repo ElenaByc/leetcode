2774260752b4b4933793cf8a3e9c07efae6bb419
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-05-21 17:24:30 -0700

    Time: 1888 ms (64.47%), Space: 40.2 MB (64.23%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -6,6 +6,9 @@ class Solution {
             if (num % i == 0) {
                 sum += i;
             }
+            if(sum > num) {
+                return false;
+            }
         }
         
         return num == sum;
