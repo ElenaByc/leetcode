83b1878721ce3b8cd2af2539b6ac42a823b7bfb9
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-07-04 21:02:53 -0700

    Time: 20 ms (5.32%), Space: 44.6 MB (16.77%) - LeetHub

--- 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.java
+++ 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.java
@@ -1,13 +1,34 @@
 class Solution {
     public int[] maxSubsequence(int[] nums, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>
-                    (Comparator.comparingInt(i -> nums[i]));
-        for (int i = 0; i < nums.length; ++i) {
-            pq.offer(i);
-            if (pq.size() > k) {
-                pq.poll();
-            }
+        int n = nums.length;
+        int[][] arr = new int[n][2];
+        int[] ans = new int[k];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = new int[] {nums[i], i};
         }
-        return pq.stream().sorted().mapToInt(i -> nums[i]).toArray();
+        Arrays.sort(arr, new Comparator<int[]>() {
+          @Override
+          public int compare(int[] p1, int[] p2) {
+            return Integer.compare(p1[0], p2[0]); // Compare by first element
+          }
+        });
+        System.out.println(Arrays.deepToString(arr));
+        int[][] arr2 = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            arr2[i] = arr[n - 1 - i];
+        }
+        Arrays.sort(arr2, new Comparator<int[]>() {
+          @Override
+          public int compare(int[] p1, int[] p2) {
+            return Integer.compare(p1[1], p2[1]); // Compare by second element
+          }
+        });
+        System.out.println(Arrays.deepToString(arr2));
+        for (int i = 0; i < k; i++) {
+            ans[i] = arr2[i][0];
+        }
+        
+        return ans;
     }
 }
