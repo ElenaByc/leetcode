fdc6fc1bccb0c55f38364f7f08cdc956f0368cc2
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-07-27 21:08:19 -0700

    Time: 0 ms (100.00%), Space: 44.3 MB (100.00%) - LeetHub

---
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canAliceWin(int[] nums) {
+        int sum1 = 0;
+        int sum2 = 0;
+        
+        for (int n : nums) {
+            if (n < 10) {
+                sum1 += n;
+            } else {
+                sum2 += n;
+            }
+        }
+        
+        return sum1 != sum2;
+    }
+}
