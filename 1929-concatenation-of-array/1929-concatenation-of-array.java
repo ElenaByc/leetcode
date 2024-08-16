c221c1b6a35e51a8435309a28a48b28a94973684
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-08-15 20:45:30 -0700

    Time: 1 ms (96.25%), Space: 45.4 MB (27.86%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.java
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -3,15 +3,11 @@ class Solution {
         int n = nums.length;
         int[] ans = new int[2 * n];
         
-        for (int i = 0; i < 2 * n; i++) {
-            if (i < n) {
-                ans[i] = nums[i];
-            } else {
-                ans[i] = nums[i - n];
-            }
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
         }
         
         return ans;
-        
     }
 }
