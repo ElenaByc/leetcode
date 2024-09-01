da5f47347786bbb5cef7992778bf99daee6f306a
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-08-31 20:18:55 -0700

    Time: 3 ms (97.73%), Space: 55.6 MB (36.41%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.java
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.java
@@ -1,16 +1,18 @@
 class Solution {
-    public int[][] construct2DArray(int[] original, int m, int n) {
-        if (original.length != m * n) {
+    public int[][] construct2DArray(int[] o, int m, int n) {
+        if (m * n != o.length) {
             return new int[][] {};
-         }
-        int[][] result = new int[m][n];
+        }
+        
+        int[][] res = new int[m][n];
+        
         int idx = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                result[i][j] = original[idx++];           
+                res[i][j] = o[idx++];
             }
         }
         
-        return result;
+        return res;
     }
 }
