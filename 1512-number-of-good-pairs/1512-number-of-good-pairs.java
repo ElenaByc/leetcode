af905655057e7fbd800e7dfaad078f02d947f229
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-09-06 20:42:15 -0700

    Time: 0 ms (100.00%), Space: 40.9 MB (50.44%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -1,14 +1,19 @@
 class Solution {
     public int numIdenticalPairs(int[] nums) {
         int result = 0;
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] == nums[j]) {
-                    result++;
-                }
-            }
+        int[] arr = new int [101];
+        
+        for (int num : nums) {
+            arr[num]++;
         }
+        
+        for (int i = 1; i <= 100; i++) {
+            if (arr[i] > 1) {
+                result += arr[i] * (arr[i] - 1) / 2;
+            } 
+        }
+        
+        
         return result;
     }
 }
