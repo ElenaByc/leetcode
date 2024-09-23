e70bf217068c51013a947a53f1935abddb99b6a8
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-09-22 18:07:19 -0700

    Time: 14 ms (89.12%), Space: 45.5 MB (21.43%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.java
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.java
@@ -1,30 +1,41 @@
+class TrieNode {
+    Map<Character, TrieNode> children = new HashMap();
+    boolean isWord = false;
+}
+
 class Solution {
     public int minExtraChar(String s, String[] dictionary) {
-        // dp[i] = minimum characters left for string[0...i-1] with length = i
-        // dp[i] = dp[i-1] + 1 (base case, if no match is found)
-        // dp[i] = min(dp[i], dp[i - len(s[i..j])]), if the substring is in dictionary
-
         int n = s.length();
-        int[] dp = new int[n + 1];
+        var root = buildTrie(dictionary);
+        var dp = new int[n + 1];
 
-        HashSet<String> dict = new HashSet<>();
-
-        // for (String str : dictionary) {
-        //     dict.add(str);
-        // }
-
-        dp[0] = 0;
-        for (int i = 1; i <= n; i++) {
-            dp[i] = dp[i - 1] + 1;
-            for (String word : dictionary) {
-                int len = word.length();
-                if (i >= len && s.substring(i - len, i).equals(word)) {
-                    dp[i] = Math.min(dp[i], dp[i - len]);
+        for (int start = n - 1; start >= 0; start--) {
+            dp[start] = dp[start + 1] + 1;
+            var node = root;
+            for (int end = start; end < n; end++) {
+                if (!node.children.containsKey(s.charAt(end))) {
+                    break;
+                }
+                node = node.children.get(s.charAt(end));
+                if (node.isWord) {
+                    dp[start] = Math.min(dp[start], dp[end + 1]);
                 }
             }
-
         }
 
-        return dp[n]; // minimum characters left for string[0...n-1] with length = n
+        return dp[0];
+    }
+
+    private TrieNode buildTrie(String[] dictionary) {
+        var root = new TrieNode();
+        for (var word : dictionary) {
+            var node = root;
+            for (var c : word.toCharArray()) {
+                node.children.putIfAbsent(c, new TrieNode());
+                node = node.children.get(c);
+            }
+            node.isWord = true;
+        }
+        return root;
     }
 }
