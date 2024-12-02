82249d7d9d64f1d62e75f3f9037f7a288a432969
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-12-01 17:10:32 -0800

    Time: 5 ms (100.00%), Space: 60.7 MB (100.00%) - LeetHub

--- 3371-identify-the-largest-outlier-in-an-array/3371-identify-the-largest-outlier-in-an-array.java
+++ 3371-identify-the-largest-outlier-in-an-array/3371-identify-the-largest-outlier-in-an-array.java
@@ -1,47 +1,30 @@
 class Solution {
     public int getLargestOutlier(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
+        int[] cnt = new int[2001];
+        int total = 0;
         
-        long total = 0;
         for (int num : nums) {
             total += num;
+            cnt[num + 1000]++;
         }
-        // System.out.println(Arrays.toString(nums));
-        // System.out.println("total = " + total);
         
-        long sum;
-        for (int i = n - 1; i >= 0; i--) {
-            int num = nums[i];
+        int sum;
+        for (int i = 2000; i >= 0; i--) {
+            if (cnt[i] == 0) {
+                continue;
+            }
+            int num = i - 1000;
             sum = total - num;
             if (sum % 2 != 0) {
                 continue;
             }
-            //binary search for sum / 2
             sum /= 2;
-            int low = 0;
-            int high = n - 1;
-            int mid;
-            while (low <= high) {
-                mid = low + (high - low) / 2;
-                if (nums[mid] == sum) {
-                    if (mid != i) {
-                        return num;
-                    } 
-                    if (mid > 0 && nums[mid - 1] == sum) {
-                        return num;
-                    }
-                    if (mid < n - 1 && nums[mid + 1] == sum) {
-                        return num;
-                    }
-                    low = high + 1;
-                } else if (nums[mid] > sum) {
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
-                }
+            if (sum < -1000 || sum > 1000) {
+                continue;
             }
-            
+            if ((sum != num && cnt[sum + 1000] > 0) || (sum == num && cnt[sum + 1000] > 1)) {
+                return num;
+            }
         }
         
         return 0;
